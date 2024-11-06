37eb92ebf15f88bf5f012a45b137f68473b4eba1
Saharach Thaiutai <saharachthaiutai@outlook.com>
2024-11-06 13:32:57 +0000

    Reorganising

--- feelingLucky.js
+++
@@ -1,4 +0,0 @@
-function feelingLucky() {
-    var query = document.querySelector('.text-search').value;
-    window.location.href = 'https://www.google.com/search?q=' + encodeURIComponent(query) + '&btnI';
- }
